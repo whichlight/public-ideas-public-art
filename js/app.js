cde38154ee8c110b02785e74d3ac44363fb2fc69
Kawandeep Virdee <whichlight@gmail.com>
2013-12-10 02:22:07 -0500

    color changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   var myBoard = new DrawingBoard.Board('playground', {
     size:5,
+    color: "rgba(20,10,255,0.5)",
     background: 'img/Graph_paper2.gif',
       DrawingMode: { filler: false},
       controls: [{
